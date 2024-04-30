0ec39ad0292d58df7b90ef5a53e566048935a5ef
Stefan Zellmann <info@szellmann.de>
2024-04-30 16:27:46 +0200

    For the moment move back to old EPS behavior

--- renderer/AO.h
+++ renderer/AO.h
@@ -31,9 +31,9 @@ inline float computeAO(ScreenSample &ss, unsigned worldID,
     vec3 dir = normalize(sp.x*u + sp.y*v + sp.z*w);
 
     Ray aoRay;
-    aoRay.ori = isectPos + Ng * eps;
+    aoRay.ori = isectPos;
     aoRay.dir = dir;
-    aoRay.tmin = 0.f;
+    aoRay.tmin = 1e-4f;
     aoRay.tmax = AO_radius;
 
     float weight = max(0.f, dot(dir,Ns));
--- renderer/DirectLight_impl.cpp
+++ renderer/DirectLight_impl.cpp
@@ -262,10 +262,10 @@ bool shade(ScreenSample &ss, Ray &ray, unsigned worldID,
     }
 
     // Convert primary to shadow ray
-    ray.ori = hitPos + sn * eps;
+    ray.ori = hitPos;
     ray.dir = normalize(ls.dir);
-    ray.tmin = 0.f;
-    ray.tmax = ls.dist-1e-4f; // TODO: bias sample point
+    ray.tmin = 1e-4f;
+    ray.tmax = ls.dist-1e-4f;
   } else { // bounceID == 1
     int surfV = hr.hit ? 0 : 1;
     int volV = hitRec.volumeHit ? 0 : 1;
