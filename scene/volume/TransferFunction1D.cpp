6dba209bd24b6af5285f21e9d3f7d3ea1de805b9
Stefan Zellmann <info@szellmann.de>
2024-09-03 20:10:26 +0200

    Avoid array reallocation if possible

--- scene/volume/TransferFunction1D.cpp
+++ scene/volume/TransferFunction1D.cpp
@@ -112,7 +112,7 @@ void TransferFunction1D::commit()
   vvol.asTransferFunction1D.numValues = tex.size()[0];
   vvol.asTransferFunction1D.valueRange = m_valueRange;
 #ifdef WITH_CUDA
-  transFuncTexture = cuda_texture<float4, 1>(tex);
+  transFuncTexture.reset(tex);
   vvol.asTransferFunction1D.sampler = cuda_texture_ref<float4, 1>(transFuncTexture);
 #elif defined(WITH_HIP)
   transFuncTexture = hip_texture<float4, 1>(tex);
