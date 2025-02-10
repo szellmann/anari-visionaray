d35aaf880f1306c7bc8ed5328ddc82b664f84d0c
Stefan Zellmann <info@szellmann.de>
2025-02-10 07:05:40 +0100

    Add getCurrentTime() helper function

--- common.h
+++ common.h
@@ -2,6 +2,25 @@
 
 // std
 #include <iostream>
+
+// os
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
+#endif
+#ifdef __GNUC__
+#include <execinfo.h>
+#include <sys/time.h>
+#endif
+
 // anari
 #include <anari/anari_cpp.hpp>
 // visionaray
@@ -76,6 +95,26 @@ inline vec4 randomColor(unsigned idx)
               (g&255)/255.f,
               (b&255)/255.f,
               1.f};
+}
+
+inline double getCurrentTime()
+{
+#ifdef _WIN32
+  SYSTEMTIME tp; GetSystemTime(&tp);
+  /*
+     Please note: we are not handling the "leap year" issue.
+ */
+  size_t numSecsSince2020
+      = tp.wSecond
+      + (60ull) * tp.wMinute
+      + (60ull * 60ull) * tp.wHour
+      + (60ull * 60ul * 24ull) * tp.wDay
+      + (60ull * 60ul * 24ull * 365ull) * (tp.wYear - 2020);
+  return double(numSecsSince2020 + tp.wMilliseconds * 1e-3);
+#else
+  struct timeval tp; gettimeofday(&tp,nullptr);
+  return double(tp.tv_sec) + double(tp.tv_usec)/1E6;
+#endif
 }
 
 struct TypeInfo
