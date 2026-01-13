6be597503d9d789ce0f968a70df8731c247ae96b
Stefan Zellmann <info@szellmann.de>
2026-01-13 17:58:45 +0100

    Favor WITH_CUDA over __CUDACC__

--- scene/volume/spatial_field/Connectivity.h
+++ scene/volume/spatial_field/Connectivity.h
@@ -14,20 +14,20 @@ namespace visionaray {
 
 namespace conn {
 
-#ifdef __CUDACC__
-__device__ __constant__
+#if defined(WITH_CUDA) || defined(WITH_HIP)
+# define CONST_DEVICE_ARRAY __device__ __constant__ static constexpr
+#else
+# define CONST_DEVICE_ARRAY static constexpr
 #endif
-static constexpr int Tet[4][3] {
+
+CONST_DEVICE_ARRAY int Tet[4][3] {
   { 1, 3, 2 },
   { 0, 2, 3 },
   { 0, 3, 1 },
   { 0, 1, 2 },
 };
 
-#ifdef __CUDACC__
-__device__ __constant__
-#endif
-static constexpr int Pyr[5][4] {
+CONST_DEVICE_ARRAY int Pyr[5][4] {
   { 0, 1, 3, 2 },
   { 0, 4, 1, -1 },
   { 2, 4, 3, -1 },
@@ -35,10 +35,7 @@ static constexpr int Pyr[5][4] {
   { 1, 4, 2, -1 },
 };
 
-#ifdef __CUDACC__
-__device__ __constant__
-#endif
-static constexpr int Wed[5][4] {
+CONST_DEVICE_ARRAY int Wed[5][4] {
   { 0, 1, 3, 4 },
   { 0, 2, 1, -1 },
   { 3, 4, 5, -1 },
@@ -46,10 +43,7 @@ static constexpr int Wed[5][4] {
   { 1, 2, 4, 5 },
 };
 
-#ifdef __CUDACC__
-__device__ __constant__
-#endif
-static constexpr int Hex[6][4] {
+CONST_DEVICE_ARRAY int Hex[6][4] {
   { 0, 4, 1, 5 },
   { 3, 2, 7, 6 },
   { 0, 3, 4, 7 },
